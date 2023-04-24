2443018bd9bd610a03d3981d361520c1c15d9c25
anand nandhu <anandnandhu52@gmail.com>
2023-04-24 12:03:32 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-resource "null_resource" "test" {
-  provisioner "local-exec" {
-    command = "echo ${var.env}"
-  }
+module "network" {
+  source = "github.com/anandnandhu1/tf-module-vpc"
+  for_each = var.vpc
+  cidr_block = each.value.cidr_block
 }
