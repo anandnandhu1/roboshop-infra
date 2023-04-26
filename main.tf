af32e6b61013a470f3bc148152be25112dffb46d
anand nandhu <anandnandhu52@gmail.com>
2023-04-26 12:00:54 +0530

    terraform

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ module "network" {
   default_vpc_id = var.default_vpc_id
 
   for_each = var.vpc
+  cidr_block           = each.value.cidr_block
   public_subnets_cidr = each.value.public_subnets_cidr
   private_subnets_cidr =each.value.private_subnets_cidr
 }
