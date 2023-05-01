ad66e00e62fcbf838d87084b999787d728c24644
anand nandhu <anandnandhu52@gmail.com>
2023-05-01 14:27:55 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-module "network" {
+module "vpc" {
   source = "github.com/anandnandhu1/tf-module-vpc"
-  env = var.env
+  env            = var.env
   default_vpc_id = var.default_vpc_id
 
-  for_each = var.vpc
-  cidr_block           = each.value.cidr_block
-  public_subnets_cidr = each.value.public_subnets_cidr
-  private_subnets_cidr =each.value.private_subnets_cidr
+  for_each          = var.vpc
+  cidr_block        = each.value.cidr_block
+  public_subnets    = each.value.public_subnets
+  private_subnets   = each.value.private_subnets
   availability_zone = each.value.availability_zone
 }
 
