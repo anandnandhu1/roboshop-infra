f81354167cfbdd7074cdf40f5fe6190a9b7ab70c
anand nandhu <anandnandhu52@gmail.com>
2023-08-03 12:30:14 +0530

    docker

--- main.tf
+++ main.tf
@@ -163,3 +163,14 @@ output "MINIKUBE_SERVER" {
 output "KUBE_CONFIG" {
   value = "scp centos@${module.minikube.public_ip}:/home/centos/kubeconfig ~/.kube/config"
 }
+
+#module "eks" {
+#  source                 = "github.com/r-devops/tf-module-eks"
+#  ENV                    = var.env
+#  PRIVATE_SUBNET_IDS     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "private_subnet_ids", null), "app", null), "subnet_ids", null)
+#  PUBLIC_SUBNET_IDS      = lookup(lookup(lookup(lookup(module.vpc, "main", null), "public_subnet_ids", null), "public", null), "subnet_ids", null)
+#  DESIRED_SIZE           = 2
+#  MAX_SIZE               = 2
+#  MIN_SIZE               = 2
+#  CREATE_PARAMETER_STORE = true
+#}
